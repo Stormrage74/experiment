16da0c0af7b0bea1d9bccb316301efba00b70069
simar <simar.gregoire@gmail.com>
2016-01-08 17:32:33 +0100

    update login view with verify logging

--- experiment/application/views/front/login.php
+++ experiment/application/views/front/login.php
@@ -26,9 +26,9 @@ action="<?php echo base_url()."front/verify"; ?>"
     <div id="login">
         <form id="form_login" name="form_login"  action="<?php echo base_url()."front/verify"; ?>" method="post">
             <label>Nom utilisateur</label>
-            <input type="text" id="login" />
+            <input type="text" name="login" />
             <label>Mot de passe</label>
-            <input type="password" id="password"/>
+            <input type="password" name="password"/>
             <br/>
             <input type="submit" value="Valider">
         </form>
