c9c111e659990b20083e5b77af1291add1615437
simar <simar.gregoire@gmail.com>
2016-01-13 18:25:31 +0100

    change verify method

--- experiment/application/controllers/controlc.php
+++ experiment/application/controllers/controlc.php
@@ -18,10 +18,44 @@ class controlc extends CI_Controller {
 	
 	public function verify()
 	{
-		foreach ($_POST as $key => $value)
+		//user identifiants
+		$user_login = "";
+		$user_psw = "";
+		
+		//user info
+		$user_name = "";
+		$user_name2 = "";
+		$user_mail = "";
+		
+		
+		if(isset($_POST))
 		{
-			echo "POST parameter are '$key' has '$value'";
+			
+			$user_login = $_POST['iuser_login'];
+			$user_psw = $_POST['iuser_psw'];
+			
+			$user_name = $_POST['iuser_name'];
+			$user_name2 = $_POST['iuser_name2'];
+			$user_mail = $_POST['iuser_mail'];
+			
+			
+// 			foreach ($_POST as $key => $value)
+// 			{
+// 				echo "POST parameter are '$key' has '$value'";
+// 			}	
 		}
+		else 
+		{
+			$this->load->view('front/subscribe');	
+		}
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 	
 	
