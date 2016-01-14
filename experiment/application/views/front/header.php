45fd505f205676077b2a8e8bec24bf0284b3427b
simar <simar.gregoire@gmail.com>
2016-01-14 16:40:41 +0100

    add header to main page with some initial loads

---
+++ experiment/application/views/front/header.php
@@ -0,0 +1,9 @@
+<?php echo doctype('html5');?>
+<html>
+    <head>	
+    	<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+		<script type="text/javascript" src="<?php echo base_url();?>assets/js/<?php echo $localjs; ?>"></script>
+		<title> <?php  echo $title ?></title>
+    </head>
+        <body>
+        	<div id="content">
